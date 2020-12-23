223e0659033500e1129ed165c5c19a6607dfa9f7
Richard Ng <richardcrng@gmail.com>
2020-12-23 17:13:14 +0000

    Add tree routes

--- server.ts
+++ server.ts
@@ -12,7 +12,11 @@ interface GitLabTreeContent {
   mode?: string;
 }
 
-app.get("/:courseId", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Try /tree/:courseId or /json/:courseId");
+});
+
+app.get("/tree/:courseId", (req, res) => {
   fs.readdir(req.params.courseId, (err, files) => {
     const content = files.map((fileName) => ({
       name: fileName,
@@ -21,6 +25,53 @@ app.get("/:courseId", (req, res) => {
     }));
     res.json(content);
   });
+});
+
+app.get("/tree/:courseId/:topicId", (req, res) => {
+  fs.readdir(
+    [req.params.courseId, req.params.topicId].join("/"),
+    (err, files) => {
+      const content = files.map((fileName) => ({
+        name: fileName,
+        path: req.params.courseId,
+        type: fileName.match(/json/) ? "blob" : "tree",
+      }));
+      res.json(content);
+    }
+  );
+});
+
+app.get("/tree/:courseId/:topicId/:chapterId", (req, res) => {
+  fs.readdir(
+    [req.params.courseId, req.params.topicId, req.params.chapterId].join("/"),
+    (err, files) => {
+      const content = files.map((fileName) => ({
+        name: fileName,
+        path: req.params.courseId,
+        type: fileName.match(/json/) ? "blob" : "tree",
+      }));
+      res.json(content);
+    }
+  );
+});
+
+app.get("/tree/:courseId/:topicId/:chapterId/:lessonId", (req, res) => {
+  fs.readdir(
+    [
+      req.params.courseId,
+      req.params.topicId,
+      req.params.chapterId,
+      req.params.lessonId,
+    ].join("/"),
+    (err, files) => {
+      const content = files.map((fileName) => ({
+        name: fileName,
+        path: req.params.courseId,
+        type: fileName.match(/json/) ? "blob" : "tree",
+      }));
+      res.json(content);
+    }
+  );
 });
 
 app.listen(4000, () => {
